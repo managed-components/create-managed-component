fb05ef17fed0e99f45739fa10f0f2b48d416eaf2
Yo'av Moshe <yoav@cloudflare.com>
2023-06-29 15:59:36 +0200

    make the newly created MC do something

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,7 +1,8 @@
 import { ComponentSettings, Manager } from '@managed-components/types'
 
-export default async function (manager: Manager, settings: ComponentSettings) {
+export default async function (manager: Manager, _settings: ComponentSettings) {
   manager.addEventListener('pageview', event => {
-    // do the things
+    console.log('Hello server!')
+    event.client.execute("console.log('Hello browser')")
   })
 }
